eeb7366bf42eb6715ea26bf8863fd1e68ae61001
RakshitKaintura <blank1951k@gmail.com>
2025-09-03 17:05:09 +0530

    Model Trained

--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,22 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a log file name with the current timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Set the path to the logs folder
+logs_path = os.path.join(os.getcwd(), "logs")
 
+# Create the logs folder if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
+
+# Set the full path for the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure the logging system
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s",
     level=logging.INFO,
 )
-
